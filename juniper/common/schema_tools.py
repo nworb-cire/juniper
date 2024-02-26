107ab88f6c06042675268efd9aa77c88107a17d8
Eric Brown <eric@ebrown.net>
2024-02-26 13:39:32 -0700

    Handle lists of non-struct item

--- juniper/common/schema_tools.py
+++ juniper/common/schema_tools.py
@@ -2,6 +2,8 @@ import json
 
 import pyarrow as pa
 
+from juniper.common.data_type import FeatureType
+
 
 def _get_flattened_fields(
     list_type: pa.lib.ListType, record_path: str, metadata: dict | None
@@ -28,9 +30,13 @@ def _get_flattened_fields(
 def get_field_schema(field: pa.lib.Field) -> pa.Schema:
     if not isinstance(field.type, pa.lib.ListType):
         raise ValueError("Field type must be a ListType")
-    if (metadata_str := field.metadata.get(b"usable_type")) != b"array":  # TODO: fix after the metadata is fixed
+    if (metadata_str := field.metadata.get(b"usable_type", b"").decode()) != FeatureType.UNUSABLE:
         metadata = json.loads(metadata_str)[0]
+        if metadata == FeatureType.UNUSABLE:
+            metadata = None
     else:
         metadata = None
+    if isinstance(metadata, str):
+        return pa.schema([field.with_metadata({"usable_type": metadata})])
     fields = _get_flattened_fields(field.type, field.name, metadata=metadata)
     return pa.schema(fields)
