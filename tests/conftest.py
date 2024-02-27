0b3f828289255d6753090e687b63f78577a70124
Eric Brown <eric@ebrown.net>
2024-02-26 22:23:03 -0700

    Fix fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import os
+from pathlib import Path
+
+import pandas as pd
+import pyarrow as pa
 
 import pytest
+from pyarrow import parquet as pq
+
+from juniper.data_loading.feature_store import ParquetFeatureStore
 
 
 @pytest.fixture
@@ -8,3 +15,19 @@ def config():
     os.environ["CONFIG_LOCATION"] = "tests/test_config.toml"
     yield
     del os.environ["CONFIG_LOCATION"]
+
+
+class FeatureStoreTest(ParquetFeatureStore):
+    def get_schema(self) -> pa.Schema:
+        return pq.read_schema(self.path)
+
+    def read_parquet(
+        self, path: Path = None, columns: list[str] = None, filters: list[tuple] | list[list[tuple]] | None = None
+    ) -> pd.DataFrame:
+        df = pd.read_parquet(self.path, columns=columns, filters=filters)
+        return df.set_index(self.index_column)
+
+
+@pytest.fixture
+def feature_store(config):
+    return FeatureStoreTest()
--- tests/test_feature_store.py
+++ tests/test_feature_store.py
@@ -1,27 +1,4 @@
-from pathlib import Path
-
 import pandas as pd
-import pyarrow as pa
-import pytest
-from pyarrow import parquet as pq
-
-from juniper.data_loading.feature_store import ParquetFeatureStore
-
-
-class FeatureStoreTest(ParquetFeatureStore):
-    def get_schema(self) -> pa.Schema:
-        return pq.read_schema(self.path)
-
-    def read_parquet(
-        self, path: Path = None, columns: list[str] = None, filters: list[tuple] | list[list[tuple]] | None = None
-    ) -> pd.DataFrame:
-        df = pd.read_parquet(self.path, columns=columns, filters=filters)
-        return df.set_index(self.index_column)
-
-
-@pytest.fixture
-def feature_store(config):
-    return FeatureStoreTest()
 
 
 def test_read_parquet(feature_store):
