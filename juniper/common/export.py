505d3bac66c9c96153e1c77b37fba6204682812b
Eric Brown <eric@ebrown.net>
2024-03-20 13:53:01 -0600

    Refactor

--- juniper/common/export.py
+++ juniper/common/export.py
@@ -3,23 +3,32 @@ from skl2onnx import convert_sklearn
 from sklearn.compose import ColumnTransformer
 
 from juniper.common.data_type import FeatureType
+from juniper.preprocessor.column_normalizer import ColumnNormalizer
+
+
+def feature_type_to_onnx_type(feature_type: FeatureType) -> TensorType:
+    match feature_type:
+        case FeatureType.ARRAY:
+            raise ValueError("Must not call this function with array type")
+        case FeatureType.NUMERIC:
+            return FloatTensorType([None, 1])
+        case FeatureType.CATEGORICAL:
+            return StringTensorType([None, 1])
+        case FeatureType.BOOLEAN:
+            return FloatTensorType([None, 1])
+        case FeatureType.TIMESTAMP:
+            raise NotImplementedError("Timestamps not yet supported")
+        case _:
+            raise ValueError(f"Unknown feature type {feature_type}")
 
 
 def get_onnx_types(column_transformer) -> list[tuple[str, TensorType]]:
     initial_types = []
-    for name, _, columns in column_transformer.transformers:
+    for name, t, columns in column_transformer.transformers:
+        if isinstance(t, ColumnNormalizer):
+            raise NotImplementedError("Array types not yet supported")
         for col in columns:
-            match name:
-                case FeatureType.NUMERIC:
-                    initial_types.append((col, FloatTensorType([None, 1])))
-                case FeatureType.CATEGORICAL:
-                    initial_types.append((col, StringTensorType([None, 1])))
-                case FeatureType.BOOLEAN:
-                    initial_types.append((col, FloatTensorType([None, 1])))
-                case FeatureType.TIMESTAMP:
-                    raise NotImplementedError("Timestamps not yet supported")
-                case _:
-                    raise ValueError(f"Unknown feature type {name}")
+            initial_types.append((col, feature_type_to_onnx_type(name)))
     return initial_types
 
 
