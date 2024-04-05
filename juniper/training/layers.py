ab39eba363a099866311fb4f8e935b9345b0e2f6
Eric Brown <eric@ebrown.net>
2024-04-04 19:27:47 -0600

    Add Unify layer

---
+++ juniper/training/layers.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import torch
+from torch import nn
+
+from juniper.training import nan_ops
+
+
+class SummaryPool(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.training:
+            return torch.cat(
+                [
+                    torch.nanmean(x, dim=-1),
+                    nan_ops.nanstd(x, dim=-1),
+                    nan_ops.nanmax(x, dim=-1)[0],
+                    nan_ops.nanmin(x, dim=-1)[0],
+                ],
+                dim=-1,
+            )
+        else:
+            return torch.cat(
+                [
+                    torch.mean(x, dim=-1),
+                    torch.std(x, dim=-1),
+                    torch.max(x, dim=-1)[0],
+                    torch.min(x, dim=-1)[0],
+                ],
+                dim=-1,
+            )
+
+
+class Unify(nn.Module):
+    def __init__(
+        self,
+        modules: dict[str, nn.Module],
+        padding_value=0.0,
+    ):
+        super().__init__()
+        self.modules = modules
+        self.padding_value = padding_value
+
+    def forward(self, x: pd.DataFrame) -> torch.Tensor:
+        assert all(name in x.columns for name in self.modules.keys())
+        ret = torch.Tensor()
+        for col, module in self.modules.items():
+            tensors = x[col].apply(torch.tensor).apply(lambda x: x.T)
+            if self.padding_value is not None:
+                y = torch.nn.utils.rnn.pad_sequence(
+                    tensors.values.tolist(), batch_first=True, padding_value=self.padding_value
+                )  # BxSxV
+                y = y.permute(0, 2, 1)  # BxVxS
+            else:
+                # Untested as torch support for nested (jagged) tensors is still experimental
+                y = torch.nested.nested_tensor(tensors.values.tolist())  # BxVxS
+            y = module(y)  # BxV
+            ret = torch.cat([ret, y], dim=-1)
+            x = x.drop(columns=[col])
+        ret = torch.cat([ret, torch.tensor(x.values, dtype=torch.float32)], dim=-1)
+        return ret
---
+++ tests/test_layers.py
@@ -0,0 +1,25 @@
+import pyarrow as pa
+import pytest
+import torch
+
+from juniper.common.data_type import FeatureType
+from juniper.preprocessor.preprocessor import get_preprocessor
+from juniper.training.layers import Unify, SummaryPool
+
+
+@pytest.fixture
+def onnx_schema(feature_store):
+    """Remove certain column types from the schema until they are ready to be supported"""
+    schema = feature_store.get_schema()
+    return pa.schema([field for field in schema if field.metadata[b"usable_type"].decode() != FeatureType.TIMESTAMP])
+
+
+def test_unify(feature_store, onnx_schema):
+    column_transformer = get_preprocessor(feature_store, schema=onnx_schema)
+    df = feature_store.read_parquet()
+    x = column_transformer.fit_transform(df)
+    unify = Unify({"arr__arr": SummaryPool()})
+    y = unify(x)
+    assert isinstance(y, torch.Tensor)
+    assert y.shape[0] == x.shape[0]
+    assert y.shape[1] == x.shape[1] - 1 + 4 * len(x["arr__arr"].iloc[0])
